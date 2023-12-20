b2054a7ea3df55a6d646b923fe2f08c02ea12792
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-20 13:41:46 +0100

    Added DB connection condition to make server work if DB is connected + added routes to server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
+
+const userRoutes = require('./routes/user-routes');
+const { dbConnect } = require('./config/db-connect');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/api/user', userRoutes);
+
+if (dbConnect) {
+    console.log('MongoDB connection success');
+  app.listen(4000);
+} else {
+    console.log('MongoDB connection failed');
+}
+
 app.get('/', (req, res, next) => {
   res.status(200).json({ message: 'Server running !' });
 });
 
-app.listen(4000);
