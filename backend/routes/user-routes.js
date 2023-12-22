5ef737caee912075476d93a1253cd0c3cbed6691
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-22 18:53:53 +0100

    Implementation of POST route for registering

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const express = require('express');
 
+const { getOneUser, createUser } = require('./../controllers/user-controller');
+
 const router = express.Router();
+
+router.post('/register', createUser);
 
 module.exports = router;
