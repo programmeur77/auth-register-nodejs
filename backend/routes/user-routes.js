19b20a725b5cd43e2c7264e1ef8314bae08cade1
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-22 20:17:04 +0100

    Imported limiter middleware

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 const { getOneUser, createUser } = require('./../controllers/user-controller');
+const { limiter } = require('./../middlewares/rate-limiter');
 
 const router = express.Router();
 
-router.get('/signin', getOneUser);
+router.get('/signin', limiter, getOneUser);
 router.post('/register', createUser);
 
 module.exports = router;
