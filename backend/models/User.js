149a40d30109219df8f7f680c0fe9200bb039bbd
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-22 19:20:21 +0100

    Changed model to add UUID for unique-key and changed createdAt type to String and added emailIsVerified entry

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  uniqueKey: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now() },
+  uniqueKey: { type: String, default: crypto.randomUUID, unique: true },
+  createdAt: { type: String, default: new Date().toLocaleString() },
+  emailIsVerified: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', userSchema);
