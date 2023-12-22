875268607b9807487402bdf28f8373aea743c1c0
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-22 19:55:24 +0100

    Added routes to sign in a user

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -5,6 +5,7 @@ const { getOneUser, createUser } = require('./../controllers/user-controller');
 
 const router = express.Router();
 
+router.get('/signin', getOneUser);
 router.post('/register', createUser);
 
 module.exports = router;
