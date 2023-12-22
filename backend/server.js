2574c9dc38f20ab8eca0ac79809d6813399c0e9a
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-22 22:39:56 +0100

    Added headers for CORS

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.setHeader(
-    'Access-Control-Allow-Header',
-    'X-Requested-With, Content, Accept, Content-Type, Authorization'
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Content-Type, Accept, Authorization'
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
   );
   next();
 });
