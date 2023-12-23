1695bf1c7e4198a8c519efa96e928274ab3ba6f0
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-23 01:02:18 +0100

    Erased limiter for the moment

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Content-Type, Accept, Authorization'
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
   );
   next();
 });
