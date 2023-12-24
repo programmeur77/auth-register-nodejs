ba967a2168d2836446b3162a57007ff2eba2ae60
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-24 08:23:25 +0100

    Added mail app.use route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const userRoutes = require('./routes/user-routes');
+const mailRoutes = require('./routes/mail-routes');
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/mail', mailRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
