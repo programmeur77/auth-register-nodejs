505325ebff3e3bdf7550580b9d0fa6eae8929920
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-24 08:34:43 +0100

    Added limiter to login route

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 const { getOneUser, createUser } = require('./../controllers/user-controller');
+const { limitRate } = require('../middlewares/rate-limiter');
 
 const router = express.Router();
 
-router.post('/signin', getOneUser);
+router.post('/signin', limitRate, getOneUser);
 router.post('/register', createUser);
 
 module.exports = router;
